a214898df9aaea58d4e3a0fdd219f0bb341b697b
Explothusist <jcedricserio@gmail.com>
2024-07-16 10:10:44 -0500

    Works pretty well, forest, fort

--- main.js
+++ main.js
@@ -8,9 +8,305 @@ m_canv.height = height;
 
 const welt_ctx = new WeltContext();
 
-welt_ctx.add_vertex(10, 0, 0);
-welt_ctx.add_vertex(-10, 0, 0);
-welt_ctx.add_vertex(0, 10, 0);
-welt_ctx.add_vertex(0, -10, 0);
+// welt_ctx.add_vertex(-200, -200, 200); // fr
+// welt_ctx.add_vertex(-200, 200, 200); // br
+// welt_ctx.add_vertex(200, 200, 200); // bl
+// welt_ctx.add_vertex(200, -200, 200); // fl
+// welt_ctx.add_vertex(-200, -200, -200);
+// welt_ctx.add_vertex(-200, 200, -200);
+// welt_ctx.add_vertex(200, 200, -200);
+// welt_ctx.add_vertex(200, -200, -200);
 
-welt_ctx.render(0, 0, width, height, m_ctx);
+// welt_ctx.add_vertex(-500, -500, 500); // fr
+// welt_ctx.add_vertex(-500, 500, 500); // br
+// welt_ctx.add_vertex(500, 500, 500); // bl
+// welt_ctx.add_vertex(500, -500, 500); // fl
+// welt_ctx.add_vertex(-500, -500, -500);
+// welt_ctx.add_vertex(-500, 500, -500);
+// welt_ctx.add_vertex(500, 500, -500);
+// welt_ctx.add_vertex(500, -500, -500);
+
+// welt_ctx.add_surface([1, 2, 6, 5], "#A00000"); // b
+// welt_ctx.add_surface([0, 1, 2, 3], "#00A000"); // t
+// welt_ctx.add_surface([4, 5, 6, 7], "#00A000"); // b
+// welt_ctx.add_surface([2, 3, 7, 6], "#0000A0"); // l
+// welt_ctx.add_surface([0, 1, 5, 4], "#0000A0"); // r
+// welt_ctx.add_surface([0, 3, 7, 4], "#A00000"); // f
+
+function add_grass(x, y, z) {
+    let far_right = welt_ctx.add_vertex(x+500, y+500, z);
+    let far_left = welt_ctx.add_vertex(x-500, y+500, z);
+    let back_right = welt_ctx.add_vertex(x+500, y-500, z);
+    let back_left = welt_ctx.add_vertex(x-500, y-500, z);
+    welt_ctx.add_floor([far_right, far_left, back_left, back_right], "#005000");
+};
+
+// add_grass(0, 0, 0);
+// add_grass(1000, 0, 0);
+// add_grass(2000, 0, 0);
+// add_grass(3000, 0, 0);
+// add_grass(4000, 0, 0);
+// add_grass(5000, 0, 0);
+// add_grass(0, -1000, 0);
+// add_grass(1000, -1000, 0);
+// add_grass(2000, -1000, 0);
+// add_grass(3000, -1000, 0);
+// add_grass(0, 1000, 0);
+// add_grass(1000, 1000, 0);
+// add_grass(2000, 1000, 0);
+// add_grass(3000, 1000, 0);
+
+for (let i = -5000; i <= 5000; i += 1000) {
+    for (let j = -3000; j <= 3000; j += 1000) {
+        add_grass(i, j, 0);
+    }
+}
+
+function add_checker_board(x, y, z) {
+    let size_of_square = 100;
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            let far_right = welt_ctx.add_vertex(x+(i*size_of_square)+(size_of_square/2), y+(j*size_of_square)+(size_of_square/2), z);
+            let far_left = welt_ctx.add_vertex(x+(i*size_of_square)-(size_of_square/2), y+(j*size_of_square)+(size_of_square/2), z);
+            let back_right = welt_ctx.add_vertex(x+(i*size_of_square)+(size_of_square/2), y+(j*size_of_square)-(size_of_square/2), z);
+            let back_left = welt_ctx.add_vertex(x+(i*size_of_square)-(size_of_square/2), y+(j*size_of_square)-(size_of_square/2), z);
+            let color = "#000000";
+            if (((i+j) % 2) === 0) {
+                color = "#FFFFFF";
+            }
+            welt_ctx.add_floor([far_right, far_left, back_left, back_right], color);
+        }
+    }
+};
+
+add_checker_board(1000, 1000, 0);
+
+function add_tree(x, y, z) {
+    let bottom_near_left = welt_ctx.add_vertex(x, y, z);
+    let bottom_near_right = welt_ctx.add_vertex(x-100, y, z);
+    let bottom_far = welt_ctx.add_vertex(x-50, y+100, z);
+    let top_near_left = welt_ctx.add_vertex(x, y, z-400);
+    let top_near_right = welt_ctx.add_vertex(x-100, y, z-400);
+    let top_far = welt_ctx.add_vertex(x-50, y+100, z-400);
+
+    welt_ctx.add_surface([bottom_near_left, bottom_near_right, bottom_far], "#905020");
+    welt_ctx.add_surface([top_near_left, top_near_right, top_far], "#905020");
+    welt_ctx.add_surface([bottom_near_left, bottom_near_right, top_near_right, top_near_left], "#905020");
+    welt_ctx.add_surface([bottom_near_left, bottom_far, top_far, top_near_left], "#905020");
+    welt_ctx.add_surface([bottom_near_right, bottom_far, top_far, top_near_right], "#905020");
+    
+    let leaf_low_near_left = welt_ctx.add_vertex(x+100, y-100, z-400);
+    let leaf_low_near_right = welt_ctx.add_vertex(x-200, y-100, z-400);
+    let leaf_low_far = welt_ctx.add_vertex(x-150, y+200, z-400);
+    let leaf_high = welt_ctx.add_vertex(x-50, y+50, z-800);
+
+    welt_ctx.add_surface([leaf_low_far, leaf_low_near_left, leaf_low_near_right], "#00A000");
+    welt_ctx.add_surface([leaf_high, leaf_low_near_left, leaf_low_near_right], "#00A000");
+    welt_ctx.add_surface([leaf_low_far, leaf_high, leaf_low_near_right], "#00A000");
+    welt_ctx.add_surface([leaf_low_far, leaf_low_near_left, leaf_high], "#00A000");
+};
+
+// add_tree(-200, 200, 0);
+// add_tree(-50, 400, 0);
+
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+        add_tree(-200-(i*500)-(Math.random()*200), 200+(j*500)+(Math.random()*200), 0);
+    }
+}
+
+function add_gate(x, y, z) {
+    let gate_left = welt_ctx.add_vertex(x-200, y+300, z);
+    let gate_right = welt_ctx.add_vertex(x+200, y+300, z);
+    let gate_top_left = welt_ctx.add_vertex(x-200, y+300, z-600);
+    let gate_top_right = welt_ctx.add_vertex(x+200, y+300, z-600);
+    let gate_back_left = welt_ctx.add_vertex(x-200, y-300, z);
+    let gate_back_right = welt_ctx.add_vertex(x+200, y-300, z);
+    let gate_back_top_left = welt_ctx.add_vertex(x-200, y-300, z-600);
+    let gate_back_top_right = welt_ctx.add_vertex(x+200, y-300, z-600);
+
+    let gate_far_left = welt_ctx.add_vertex(x-500, y+300, z);
+    let gate_far_right = welt_ctx.add_vertex(x+500, y+300, z);
+    let gate_far_top_left = welt_ctx.add_vertex(x-500, y+300, z-800);
+    let gate_far_top_right = welt_ctx.add_vertex(x+500, y+300, z-800);
+    let gate_far_back_left = welt_ctx.add_vertex(x-500, y-300, z);
+    let gate_far_back_right = welt_ctx.add_vertex(x+500, y-300, z);
+    let gate_far_back_top_left = welt_ctx.add_vertex(x-500, y-300, z-800);
+    let gate_far_back_top_right = welt_ctx.add_vertex(x+500, y-300, z-800);
+
+    welt_ctx.add_surface([gate_left, gate_top_left, gate_back_top_left, gate_back_left], "#505050");
+    welt_ctx.add_surface([gate_right, gate_top_right, gate_back_top_right, gate_back_right], "#505050");
+    welt_ctx.add_surface([gate_top_left, gate_top_right, gate_back_top_right, gate_back_top_left], "#505050");
+    
+    welt_ctx.add_surface([gate_left, gate_far_left, gate_far_top_left, gate_top_left], "#707070");
+    welt_ctx.add_surface([gate_right, gate_far_right, gate_far_top_right, gate_top_right], "#707070");
+    welt_ctx.add_surface([gate_top_left, gate_far_top_left, gate_far_top_right, gate_top_right], "#707070");
+    welt_ctx.add_surface([gate_back_left, gate_far_back_left, gate_far_back_top_left, gate_back_top_left], "#707070");
+    welt_ctx.add_surface([gate_back_right, gate_far_back_right, gate_far_back_top_right, gate_back_top_right], "#707070");
+    welt_ctx.add_surface([gate_back_top_left, gate_far_back_top_left, gate_far_back_top_right, gate_back_top_right], "#707070");
+    
+    welt_ctx.add_surface([gate_far_left, gate_far_top_left, gate_far_back_top_left, gate_far_back_left], "#707070");
+    welt_ctx.add_surface([gate_far_right, gate_far_top_right, gate_far_back_top_right, gate_far_back_right], "#707070");
+    welt_ctx.add_surface([gate_far_top_left, gate_far_top_right, gate_far_back_top_right, gate_far_back_top_left], "#707070");
+};
+function add_wall(x, y, z) {
+    let wall_left = welt_ctx.add_vertex(x-500, y+150, z);
+    let wall_right = welt_ctx.add_vertex(x+500, y+150, z);
+    let wall_top_left = welt_ctx.add_vertex(x-500, y+150, z-600);
+    let wall_top_right = welt_ctx.add_vertex(x+500, y+150, z-600);
+    let wall_back_left = welt_ctx.add_vertex(x-500, y-150, z);
+    let wall_back_right = welt_ctx.add_vertex(x+500, y-150, z);
+    let wall_back_top_left = welt_ctx.add_vertex(x-500, y-150, z-600);
+    let wall_back_top_right = welt_ctx.add_vertex(x+500, y-150, z-600);
+    
+    welt_ctx.add_surface([wall_left, wall_top_left, wall_back_top_left, wall_back_left], "#707070");
+    welt_ctx.add_surface([wall_right, wall_top_right, wall_back_top_right, wall_back_right], "#707070");
+    welt_ctx.add_surface([wall_top_left, wall_top_right, wall_back_top_right, wall_back_top_left], "#707070");
+    welt_ctx.add_surface([wall_left, wall_top_left, wall_top_right, wall_right], "#707070");
+    welt_ctx.add_surface([wall_back_left, wall_back_top_left, wall_back_top_right, wall_back_right], "#707070");
+};
+function add_fort_front(x, y, z) {
+    add_gate(x, y, z);
+    add_wall(x-1000, y, z);
+    add_wall(x-2000, y, z);
+    add_wall(x+1000, y, z);
+    add_wall(x+2000, y, z);
+};
+
+add_fort_front(-500, -2000, 0);
+
+let key_states = {};
+function set_key_state(key, state) {
+    console.log(key);
+    switch (key) {
+        case "ArrowUp":
+            key_states.arrup = state;
+            break;
+        case "ArrowDown":
+            key_states.arrdown = state;
+            break;
+        case "ArrowLeft":
+            key_states.arrleft = state;
+            break;
+        case "ArrowRight":
+            key_states.arrright = state;
+            break;
+        case "a":
+            key_states.a = state;
+            break;
+        case "d":
+            key_states.d = state;
+            break;
+        case "s":
+            key_states.s = state;
+            break;
+        case "w":
+            key_states.w = state;
+            break;
+        case "'":
+            key_states.single_quote = state;
+            break;
+        case "/":
+            key_states.foreslash = state;
+            break;
+        case "PageUp":
+            key_states.pgup = state;
+            break;
+        case "PageDown":
+            key_states.pgdown = state;
+            break;
+        case "Control":
+            key_states.ctrl = state;
+            break;
+    }
+};
+function get_key_state(key) {
+    switch (key) {
+        case "ArrowUp":
+            return key_states.arrup;
+        case "ArrowDown":
+            return key_states.arrdown;
+        case "ArrowLeft":
+            return key_states.arrleft;
+        case "ArrowRight":
+            return key_states.arrright;
+        case "a":
+            return key_states.a;
+        case "d":
+            return key_states.d;
+        case "s":
+            return key_states.s;
+        case "w":
+            return key_states.w;
+        case "'":
+            return key_states.single_quote;
+        case "/":
+            return key_states.foreslash;
+        case "PageUp":
+            return key_states.pgup;
+        case "PageDown":
+            return key_states.pgdown;
+        case "Control":
+            return key_states.ctrl;
+    }
+};
+document.addEventListener("keydown", function(event) {
+    set_key_state(event.key, true);
+});
+document.addEventListener("keyup", function(event) {
+    set_key_state(event.key, false);
+});
+
+let move_speed = 12;
+let rotate_speed = 0.017*5;
+
+function handle_camera() {
+    if (get_key_state("ArrowUp")) {
+        // welt_ctx.camera.point.z -= 1;
+        welt_ctx.camera.moveRelative(new Vertex(0, 0, -move_speed));
+    }
+    if (get_key_state("ArrowDown")) {
+        // welt_ctx.camera.point.z += 1;
+        welt_ctx.camera.moveRelative(new Vertex(0, 0, move_speed));
+    }
+    if (get_key_state("ArrowLeft")) {
+        welt_ctx.camera.angle.theta += rotate_speed;
+    }
+    if (get_key_state("ArrowRight")) {
+        welt_ctx.camera.angle.theta -= rotate_speed;
+    }
+    if (get_key_state("w")) {
+        // welt_ctx.camera.point.y += 1;
+        welt_ctx.camera.moveRelative(new Vertex(0, move_speed, 0));
+    }
+    if (get_key_state("s")) {
+        // welt_ctx.camera.point.y -= 1;
+        welt_ctx.camera.moveRelative(new Vertex(0, -move_speed, 0));
+    }
+    if (get_key_state("a")) {
+        // welt_ctx.camera.point.x -= 1;
+        welt_ctx.camera.moveRelative(new Vertex(-move_speed, 0, 0));
+    }
+    if (get_key_state("d")) {
+        // welt_ctx.camera.point.x += 1;
+        welt_ctx.camera.moveRelative(new Vertex(move_speed, 0, 0));
+    }
+    if (get_key_state("PageUp") || get_key_state("'")) {
+        welt_ctx.camera.angle.phi += rotate_speed;
+    }
+    if (get_key_state("PageDown") || get_key_state("/")) {
+        welt_ctx.camera.angle.phi -= rotate_speed;
+    }
+};
+
+function render_loop() {
+    handle_camera();
+
+    welt_ctx.render(0, 0, width, height, m_ctx);
+
+    m_ctx.fillStyle = "#000000";
+    m_ctx.fillText("("+welt_ctx.camera.point.x+", "+welt_ctx.camera.point.y+", "+welt_ctx.camera.point.z+")", 20, 20);
+    m_ctx.fillText("("+welt_ctx.camera.angle.theta+", "+welt_ctx.camera.angle.phi+")", 20, 40);
+};
+
+setInterval(render_loop, 16);
--- weltmacher.js
+++ weltmacher.js
@@ -20,20 +20,24 @@ class Spherical_Vertex {
         this.phi *= -1;
     }
     to_cartesean() {
-        let z = this.r*Math.sin(this.phi);
-        let half_r = this.r*Math.cos(this.phi);
-        let x = half_r*Math.sin(this.theta);
-        let y = half_r*Math.cos(this.theta);
+        let z = this.r*Math.cos(this.phi);
+        let x = this.r*Math.cos(this.theta)*Math.sin(this.phi);
+        let y = this.r*Math.sin(this.theta)*Math.sin(this.phi);
         return new Vertex(x, y, z);
     }
     copy() {
         return new Spherical_Vertex(this.r, this.theta, this.phi);
     }
 
-    to_xy() {
-        let x = this.r*(Math.sin(this.theta));
-        let y = this.r*(Math.sin(this.phi));
-        return {x: x, y: y, r: this.r};
+    // to_xy() {
+    //     let x = this.r*(Math.sin(this.theta));
+    //     let y = this.r*(Math.sin(this.phi));
+    //     return {x: x, y: y, r: this.r};
+    // }
+
+    debug_text(txt, x, y, ctx) {
+        ctx.fillStyle = "rgb(0, 0, 0)";
+        ctx.fillText(txt+"("+this.r+", "+this.theta+", "+this.phi+")", x, y);
     }
 };
 
@@ -63,23 +67,42 @@ class Vertex {
         this.z *= point.z;
     }
     to_spherical() {
-        let half_r = Math.sqrt((this.y*this.y)+(this.x*this.x));
-        let r = Math.sqrt((half_r*half_r)+(this.z*this.z));
+        let r = Math.sqrt((this.x*this.x)+(this.y*this.y)+(this.z*this.z));
         let theta = Math.atan2(this.y, this.x);
-        let phi = Math.atan2(this.z, half_r);
+        let phi = Math.acos(this.z/r);
         return new Spherical_Vertex(r, theta, phi);
     }
     copy() {
         return new Vertex(this.x, this.y, this.z);
     }
+
+    add_perspective(vanish, los) {
+        let x = this.x;
+        let y = this.y;
+        let z = this.z;
+        let multiplier = (vanish-this.y)/vanish;
+        let los_mult = (vanish-los)/vanish;
+        multiplier = Math.max(los_mult, Math.min(1, multiplier));
+        // multiplier *= multiplier;
+        this.x = x*multiplier;
+        this.y = z*multiplier;
+        this.z = y;
+    }
+
+    debug_text(txt, x, y, ctx) {
+        ctx.fillStyle = "rgb(0, 0, 0)";
+        ctx.fillText(txt+"("+this.x+", "+this.y+", "+this.z+")", x, y);
+    }
 };
 
 class Surface {
     vertices = [];
+    color;
 
-    constructor(vertices) {
+    constructor(vertices, color) {
         // vertices is a list of the indexes of the vertices it is between
         this.vertices = vertices;
+        this.color = color;
     }
 };
 
@@ -91,16 +114,33 @@ class Camera {
         this.point = new Vertex(x, y, z);
         this.angle = new Spherical_Vertex(0, theta, phi);
     }
+
+    moveRelative(vector) {
+        let trans_point = this.point.to_spherical();
+        let invert_angle = this.angle.copy();
+        invert_angle.invert();
+        trans_point.translate(invert_angle);
+        trans_point = trans_point.to_cartesean();
+        trans_point.translate(vector);
+        trans_point = trans_point.to_spherical();
+        trans_point.translate(this.angle);
+        trans_point = trans_point.to_cartesean();
+        this.point = trans_point;
+    }
 }
 
 class WeltContext {
     vertices = [];
     surfaces = [];
+    floor_surfaces = [];
     camera;
     light;
+    vanish = 5000;
+    los = 3000;
 
     constructor() {
-        this.camera = new Camera(0, 0, 0, 0, 0);
+        // this.camera = new Camera(0, -400, 0, 0, 0);
+        this.camera = new Camera(0, 0, -300, 0, 0);
     }
     reset_context() {
         // Clears the entire screen
@@ -113,8 +153,11 @@ class WeltContext {
         this.vertices.push(new Vertex(x, y, z));
         return this.vertices.length-1;
     }
-    add_surface(vertices) {
-        this.surfaces.push(new Surface(vertices));
+    add_surface(vertices, color) {
+        this.surfaces.push(new Surface(vertices, color));
+    }
+    add_floor(vertices, color) {
+        this.floor_surfaces.push(new Surface(vertices, color));
     }
 
     render(x, y, width, height, ctx) {
@@ -129,22 +172,79 @@ class WeltContext {
         let cam_angle = this.camera.angle.copy();
         cam_angle.invert();
         let vertex_positions = [];
-        for (let vertex of this.vertices) {
-            let vert = vertex.copy();
+        for (let i in this.vertices) {
+            let vert = this.vertices[i].copy();
+            // vert.debug_text("start: ", 50, 100+(i*150), ctx);
             // Translates so camera point is origin
             vert.translate(cam_point);
+            // vert.debug_text("translate: ", 50, 120+(i*150), ctx);
             vert = vert.to_spherical();
+            // vert.debug_text("spherical: ", 50, 140+(i*150), ctx);
             // Rotates so camera angle is origin
             vert.translate(cam_angle);
+            // vert.debug_text("rotate: ", 50, 160+(i*150), ctx);
             // If any math is wrong, it is probably this math
-            vertex_positions.push(vert.to_xy());
+            // I was right! Now maybe it is correct
+            vert = vert.to_cartesean();
+            // vert.debug_text("cartesean: ", 50, 180+(i*150), ctx);
+            vert.add_perspective(this.vanish, this.los);
+            // vert.debug_text("perspective: ", 50, 200+(i*150), ctx);
+            vertex_positions.push(vert);
         }
 
-        console.log(vertex_positions);
+        // console.log(vertex_positions);
 
         ctx.fillStyle = "#000000";
         for (let vertex of vertex_positions) {
             ctx.fillRect(center_x+vertex.x, center_y+vertex.y, 1, 1);
+        }
+
+        for (let surface of this.floor_surfaces) {
+            ctx.fillStyle = surface.color;
+            ctx.beginPath();
+            ctx.moveTo(center_x+vertex_positions[surface.vertices[0]].x, center_y+vertex_positions[surface.vertices[0]].y);
+            for (let i = 1; i < surface.vertices.length; i++) {
+                ctx.lineTo(center_x+vertex_positions[surface.vertices[i]].x, center_y+vertex_positions[surface.vertices[i]].y);
+            }
+            ctx.closePath();
+            ctx.stroke();
+            ctx.fill();
+        }
+
+        let surfaces_sorted = [];
+        for (let i in this.surfaces) {
+            let surface = this.surfaces[i];
+            let average_dist = 0;
+            // let min_dist = this.los;
+            let one_on_screen = false;
+            for (let vert_index of surface.vertices) {
+                average_dist += vertex_positions[vert_index].z;
+                // min_dist = Math.min(vertex_positions[vert_index].z, min_dist);
+                if (vertex_positions[vert_index].z > 0 && vertex_positions[vert_index].z < this.los/* && vertex_positions[vert_index].x > -(width/2) && vertex_positions[vert_index].x < (width/2)
+                        && vertex_positions[vert_index].y > -(height/2) && vertex_positions[vert_index].y < (height/2)*/) {
+                    one_on_screen = true;
+                }
+            }
+            average_dist /= surface.vertices.length;
+            if (one_on_screen) {
+                surfaces_sorted.push({index: i, dist: average_dist});
+                // surfaces_sorted.push({index: i, dist: min_dist});
+            }
+        }
+
+        surfaces_sorted.sort((a,b) => b.dist-a.dist);
+
+        for (let surf_index of surfaces_sorted) {
+            let surface = this.surfaces[surf_index.index];
+            ctx.fillStyle = surface.color;
+            ctx.beginPath();
+            ctx.moveTo(center_x+vertex_positions[surface.vertices[0]].x, center_y+vertex_positions[surface.vertices[0]].y);
+            for (let i = 1; i < surface.vertices.length; i++) {
+                ctx.lineTo(center_x+vertex_positions[surface.vertices[i]].x, center_y+vertex_positions[surface.vertices[i]].y);
+            }
+            ctx.closePath();
+            ctx.stroke();
+            ctx.fill();
         }
     }
 };
